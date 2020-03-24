df59840f234d4ec089c29e32b02d880606876c25
Kausthub8 <47001638+Dude-1999@users.noreply.github.com>
2020-03-24 14:47:34 +0530

    Add files via upload
    A statistical overview of the given dataset

---
+++ DataExploration.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[29]:
+
+
+import numpy as np 
+import pandas as pd 
+import matplotlib as plt
+
+
+# In[14]:
+
+
+date = pd.read_csv('/home/madhuri/Downloads/hrdataset_csv')
+
+
+# In[15]:
+
+
+date
+
+
+# In[16]:
+
+
+date.head()
+
+
+# In[7]:
+
+
+date.tail()
+
+
+# In[8]:
+
+
+np.NaN
+
+
+# In[17]:
+
+
+date.dtypes()
+
+
+# In[10]:
+
+
+df.dtypes
+
+
+# In[11]:
+
+
+df.describe()
+
